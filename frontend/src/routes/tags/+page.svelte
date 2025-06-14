2f51bbbfa05d99ca2dff6feb8fec0d1243321279
ZerGo0 <18653821+ZerGo0@users.noreply.github.com>
2025-06-14 10:58:11 +0200

    ref: only show the date for last updated

--- frontend/src/routes/tags/+page.svelte
+++ frontend/src/routes/tags/+page.svelte
@@ -139,9 +139,7 @@
     return dateObj.toLocaleDateString('en-US', {
       year: 'numeric',
       month: 'short',
-      day: 'numeric',
-      hour: '2-digit',
-      minute: '2-digit'
+      day: 'numeric'
     });
   }
 
